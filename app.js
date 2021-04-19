c12967114297e405330f760d6b8dd3ecb683c45d
Lewis Nozaki <lhnozaki@outlook.com>
2021-04-18 21:28:20 -1000

    add functionality for creating grid button and some styling

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-console.log("hello world");
+'use strict'
+
+let grid = document.getElementsByClassName("grid-container")[0];
+
+let nBlocks = document.getElementById("nBlocks");
+
+let createBlocksBtn = document.getElementById("create-blocks");
+
+const createGrid = () => {
+  let numBlocks = parseInt(nBlocks.value);
+
+  for (let i = 0; i < numBlocks; i++) {
+    let newBlock = document.createElement("div");
+    newBlock.classList.add("block");
+    grid.appendChild(newBlock);
+  }
+};
+
+createBlocksBtn.addEventListener("click", createGrid);
