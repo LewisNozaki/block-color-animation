d17aeef9275a96f4e10d35fe84dc15c4f7823708
Lewis Nozaki <lhnozaki@outlook.com>
2021-04-19 21:10:31 -1000

    added color logic

--- app.js
+++ app.js
@@ -26,10 +26,57 @@ createBlocksBtn.addEventListener("click", createGrid);
 // Color Blocks Function //
 ///////////////////////////
 
+let element = document.getElementsByClassName("block");
+
 let timer = null;
 
 let counter = 0;
 
-let limit = document.getElementsByClassName("block").length;
+let limit = element.length;
 
 let fill = true;
+
+const addColor = (box) => box.classList.add("on");
+
+const removeColor = (box) => box.classList.remove("on");
+
+const move = () => {
+  timer = setTimeout(() => {
+    fill = limit === 0 ? !fill : fill;
+
+    limit = limit === 0 ? element.length : limit;
+
+    if (counter > 0) {
+      if (fill) {
+        removeColor(element[counter - 1]);
+      } else if (!fill) {
+        addColor(element[counter - 1]);
+      }
+    }
+
+    if (fill) {
+      addColor(element[counter]);
+    } else if (!fill) {
+      removeColor(element[counter]);
+    }
+
+    counter++;
+
+    if (counter < limit) {
+      move();
+    } else if (counter === limit) {
+      counter = 0;
+      limit--;
+      move();
+    }
+  }, 100);
+};
+
+const stop = () => clearInterval(timer);
+
+const reset = () => {
+  [...element].forEach(removeColor);
+  counter = 0;
+  limit = element.length;
+  fill = true;
+};
