3f18024749813b2976d04d71d40c814f878136bb
Lewis Nozaki <lhnozaki@outlook.com>
2021-04-19 21:39:39 -1000

    added stop button functionality

--- app.js
+++ app.js
@@ -26,13 +26,11 @@ createBlocksBtn.addEventListener("click", createGrid);
 // Color Blocks Function //
 ///////////////////////////
 
-let element = document.getElementsByClassName("block");
-
-let timer = null;
+let block = document.getElementsByClassName("block");
 
 let counter = 0;
 
-let limit = element.length;
+let limit = block.length;
 
 let fill = true;
 
@@ -40,24 +38,28 @@ const addColor = (box) => box.classList.add("on");
 
 const removeColor = (box) => box.classList.remove("on");
 
+let timer = null;
 const move = () => {
   timer = setTimeout(() => {
-    fill = limit === 0 ? !fill : fill;
+    // Unfill the blocks
+    if (limit === 0) {
+      fill = !fill;
+      limit = block.length;
+    };
 
-    limit = limit === 0 ? element.length : limit;
-
+    // Removes or Adds color after each iteration
     if (counter > 0) {
       if (fill) {
-        removeColor(element[counter - 1]);
-      } else if (!fill) {
-        addColor(element[counter - 1]);
+        removeColor([...block][counter - 1]);
+      } else {
+        addColor([...block][counter - 1]);
       }
     }
 
     if (fill) {
-      addColor(element[counter]);
+      addColor(block[counter]);
     } else if (!fill) {
-      removeColor(element[counter]);
+      removeColor(block[counter]);
     }
 
     counter++;
@@ -69,11 +71,21 @@ const move = () => {
       limit--;
       move();
     }
-  }, 100);
+  }, 200);
 };
 
+const moveBtn = document.getElementById("move");
+
+moveBtn.addEventListener("click", move)
+
+// Stop Button
 const stop = () => clearInterval(timer);
 
+const stopBtn = document.getElementById("stop");
+
+stopBtn.addEventListener("click", stop);
+
+// Reset Button
 const reset = () => {
   [...element].forEach(removeColor);
   counter = 0;
